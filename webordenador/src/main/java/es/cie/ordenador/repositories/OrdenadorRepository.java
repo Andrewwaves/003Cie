7831748bbdcd6463d0c076e69b8c58b2a287d512
Andrewwaves <andreacanalesmartin2000@gmail.com>
2025-01-30 09:30:07 +0100

    Add files via upload
    Proyecto personal funciona super perfect

---
+++ webordenador/src/main/java/es/cie/ordenador/repositories/OrdenadorRepository.java
@@ -0,0 +1,12 @@
+package es.cie.ordenador.repositories;
+
+import java.util.List;
+
+import es.cie.ordenador.negocio.Ordenador;
+
+public interface OrdenadorRepository {
+	
+	List <Ordenador> buscarTodos();
+	void insertar (Ordenador ordenador);
+
+}
